0cdfb613bb624516ac4534414782c002e9d3a00c
Mark Barr <mark@hyperingenuity.com>
2018-07-11 23:51:51 -0400

    Initial CodeMirror component experiments.

---
+++ src/components/CodeMirror.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="skyhook-codemirror">
+  <textarea ref="textarea" :placeholder="placeholder"></textarea>
+</div>
+</template>
+
+<script>
+import CodeMirror from 'codemirror/lib/codemirror.js'
+import 'codemirror/lib/codemirror.css'
+
+import 'codemirror/mode/javascript/javascript'
+
+import 'codemirror/theme/lesser-dark.css'
+
+export default {
+  name: 'codemirror',
+  props: {
+  },
+  data () {
+    return {
+      content: '',
+      instance: null,
+      placeholder: 'nothing here'
+    }
+  },
+  mounted () {
+    this.instance = CodeMirror.fromTextArea(this.$refs.textarea, {
+      lineNumbers: true,
+      mode: 'javascript',
+      smartIndent: true,
+      tabSize: 2,
+      theme: 'lesser-dark'
+    })
+  }
+}
+</script>
+
+<style scoped>
+.skyhook-codemirror {
+    width: 60%
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,9 @@
     <skyhook-toolbar>
       <span slot="title"><v-icon>mdi-view-dashboard-outline</v-icon> Dashboard</span>
     </skyhook-toolbar>
+
+    <codemirror></codemirror>
+
     <v-footer app fixed v-if="state.loggedIn">
       <span class="pl-2">&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -12,11 +15,13 @@
 <script>
 import store from '../store'
 import Toolbar from '../components/Toolbar.vue'
+import CodeMirror from '../components/CodeMirror.vue'
 
 export default {
   name: 'skyhook-dashboard',
   components: {
-    'skyhook-toolbar': Toolbar
+    'skyhook-toolbar': Toolbar,
+    'codemirror': CodeMirror
   },
   props: {
   },
